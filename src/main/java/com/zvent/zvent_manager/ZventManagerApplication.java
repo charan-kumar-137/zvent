764869059d849835218d053c5d5ac649fc9ba936
Charan Kumar N <charankumar137@gmail.com>
2024-10-22 18:14:26 +0530

    Initialization

---
+++ src/main/java/com/zvent/zvent_manager/ZventManagerApplication.java
@@ -0,0 +1,13 @@
+package com.zvent.zvent_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZventManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zvent/zvent_manager/ZventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zvent.zvent_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
